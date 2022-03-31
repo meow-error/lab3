0e4dc3b08f3ee082283668f4a2edf38930392946
meow-error <egorlevadniy2002@gmail.com>
2022-03-31 14:47:23 +0300

    final v1.1

--- src/main/java/com/example/lab3/Controller.java
+++ src/main/java/com/example/lab3/Controller.java
@@ -36,25 +36,29 @@ public class Controller {
 
     public void initialize(ArrayList<Reactor> reactorArrayList) {
 
-        TreeItem<String> rootItem = new TreeItem<>("Reactors");
+        TreeItem<String> rootItem = new TreeItem<>("Error");
+        if (reactorArrayList.size() != 0) {
 
-        for (Reactor reactor : reactorArrayList) {
+            rootItem = new TreeItem<>("Reactors");
 
-            TreeItem<String> branchItem = new TreeItem<>(reactor.getName());
+            for (Reactor reactor : reactorArrayList) {
 
-            TreeItem<String> leafItem1 = new TreeItem<>("name : " + reactor.getName());
-            TreeItem<String> leafItem2 = new TreeItem<>("burnup : " + reactor.getBurnup());
-            TreeItem<String> leafItem3 = new TreeItem<>("kpd : " + reactor.getKpd());
-            TreeItem<String> leafItem4 = new TreeItem<>("enrichment : " + reactor.getEnrichment());
-            TreeItem<String> leafItem5 = new TreeItem<>("termal_capacity : " + reactor.getTermal_capacity());
-            TreeItem<String> leafItem6 = new TreeItem<>("electrical_capacity : " + reactor.getElectrical_capacity());
-            TreeItem<String> leafItem7 = new TreeItem<>("life_time : " + reactor.getLife_time());
-            TreeItem<String> leafItem8 = new TreeItem<>("first_load : " + reactor.getFirst_load());
-            TreeItem<String> leafItem9 = new TreeItem<>("source : " + reactor.getSource());
+                TreeItem<String> branchItem = new TreeItem<>(reactor.getName());
 
-            branchItem.getChildren().addAll(leafItem1, leafItem2, leafItem3, leafItem4,
-                    leafItem5, leafItem6, leafItem7, leafItem8, leafItem9);
-            rootItem.getChildren().add(branchItem);
+                TreeItem<String> leafItem1 = new TreeItem<>("name : " + reactor.getName());
+                TreeItem<String> leafItem2 = new TreeItem<>("burnup : " + reactor.getBurnup());
+                TreeItem<String> leafItem3 = new TreeItem<>("kpd : " + reactor.getKpd());
+                TreeItem<String> leafItem4 = new TreeItem<>("enrichment : " + reactor.getEnrichment());
+                TreeItem<String> leafItem5 = new TreeItem<>("termal_capacity : " + reactor.getTermal_capacity());
+                TreeItem<String> leafItem6 = new TreeItem<>("electrical_capacity : " + reactor.getElectrical_capacity());
+                TreeItem<String> leafItem7 = new TreeItem<>("life_time : " + reactor.getLife_time());
+                TreeItem<String> leafItem8 = new TreeItem<>("first_load : " + reactor.getFirst_load());
+                TreeItem<String> leafItem9 = new TreeItem<>("source : " + reactor.getSource());
+
+                branchItem.getChildren().addAll(leafItem1, leafItem2, leafItem3, leafItem4,
+                        leafItem5, leafItem6, leafItem7, leafItem8, leafItem9);
+                rootItem.getChildren().add(branchItem);
+            }
         }
         treeReactors.setRoot(rootItem);
     }
@@ -63,7 +67,7 @@ public class Controller {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle("Error");
         alert.setHeaderText(null);
-        alert.setContentText("Ашипка");
+        alert.setContentText("Error");
         alert.showAndWait();
     }
 }
